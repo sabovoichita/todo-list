bd43ff68a16c8d51ba4e1c9d1dafd74f3e48e234
Voichita Maria <sabo_voichita@yahoo.com>
2024-06-18 11:29:00 +0200

    createContainer, insertContainer, initEvents (main structure)

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+function $(selector) {
+  return document.querySelector(selector);
+}
+
 function createContainer() {
   return `<div class="todo-container">
       <h1>ToDo List</h1>
       <input type="text" id="taskInput" placeholder="Add a new task" />
-      <button>Add Task</button>
+      <button id="addButton">Add Task</button>
       <ul id="taskList"></ul>
     </div>`;
 }
+
 function insertContainer() {
   document.body.innerHTML = createContainer();
 }
+
 function initEvents() {
   insertContainer();
 }
+
 initEvents();
